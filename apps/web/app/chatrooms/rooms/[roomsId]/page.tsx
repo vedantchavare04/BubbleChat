d5c5b147ffa2134e5ef0fc4038fb1f3fa1a21878
Vedant Chavare <Vedantchavare04@gmail.com>
2026-01-10 19:38:15 +0530

    core_ui modify

--- apps/web/app/chatrooms/rooms/[roomsId]/page.tsx
+++ apps/web/app/chatrooms/rooms/[roomsId]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { cn } from "@/lib/utils"
 import ModeToggle from "@/components/core_ui/mode_toggle"
+import Loader from "@/components/core_ui/loader_specific"
 
 type Message = {
   id: number
@@ -24,24 +25,7 @@ export default function RoomPage() {
 
   if (!roomsId) {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center gap-4 rounded-2xl border bg-card/60 backdrop-blur-xl px-8 py-6 shadow-lg">
-
-        <p className="text-sm font-medium text-foreground">
-          Joining room
-        </p>
-
-        <div className="flex items-center gap-1">
-          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse" />
-          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse [animation-delay:150ms]" />
-          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse [animation-delay:300ms]" />
-        </div>
-
-        <p className="text-xs text-muted-foreground">
-          Preparing a cozy space for you
-        </p>
-      </div>
-    </div>
+    <Loader />
   )
 }
 
---
+++ apps/web/components/core_ui/loader_specific.tsx
@@ -0,0 +1,22 @@
+export default function Loader(){
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4 rounded-2xl border bg-card/60 backdrop-blur-xl px-8 py-6 shadow-lg">
+
+        <p className="text-sm font-medium text-foreground">
+          Joining room
+        </p>
+
+        <div className="flex items-center gap-1">
+          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse" />
+          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse [animation-delay:150ms]" />
+          <span className="h-1.5 w-1.5 rounded-full bg-muted-foreground animate-pulse [animation-delay:300ms]" />
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Preparing a cozy space for you
+        </p>
+      </div>
+    </div>
+    )
+}
